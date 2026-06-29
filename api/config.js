ab3e8fcdc99721fbb9575af88bfbed9105c371e9
junhk-yeah <151552295+junhk-yeah@users.noreply.github.com>
2026-06-29 22:10:10 +0900

    Delete api directory

--- api/config.js
+++
@@ -1,7 +0,0 @@
-// api/config.js
-// 클라이언트가 OAuth 시작에 필요한 client_id만 반환 (secret은 반환 안 함)
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({ clientId: process.env.GOOGLE_CLIENT_ID });
-}
- 
